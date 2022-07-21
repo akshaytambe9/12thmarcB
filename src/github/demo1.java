08c09b9eb17281eb1c4d7bfc50720011a0b01ec4
rajatmachade <rajatmachade@localhost>
2022-07-21 13:36:56 +0530

    verify now

--- src/github/demo1.java
+++ src/github/demo1.java
@@ -2,9 +2,15 @@ package github;
 
 public class demo1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
+	
+			
+			System.out.println("I am Tester");
+			
+		}
 		// TODO Auto-generated method stub
 
 	}
 
-}
+
