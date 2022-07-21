10e16933fa946f15465df903ea300373ef0fb8d6
rajatmachade <rajatmachade@localhost>
2022-07-22 00:04:09 +0530

    VerifyNow

--- src/github/Sample1.java
+++ src/github/Sample1.java
@@ -5,6 +5,7 @@ public class Sample1 {
 	public static void main(String[] args)
 	{
 		System.out.println("java selenium automation testng");
+		System.out.println("Velocity Testing classes");
 
 	}
 
